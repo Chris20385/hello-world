e85e3c65799c0aa4f889e1b728f19ba26ab4bcda
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-10-08 15:40:06 -0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,10 @@
+#Cristian Garcia
+#10/08/2024
+
+#This program asks user for name and prints it
+
+name = input("What's your name? ")
+if name == "Cristian" or name == "Ben":
+    print("Hello, " + name + "!")
+else:
+    print("you are not welcome here")
